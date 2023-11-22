49cc02637f13edf57255d9a6e45d77dcd43a8708
Diego Oliveira de Araujo <diego.dearaujo@ambevtech.com.br>
2023-11-22 17:40:29 -0300

    cria um hook, separando a primeira parte de obter dados

--- src/pages/ActiveUsersList/index.tsx
+++ src/pages/ActiveUsersList/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { User } from "./types";
 
-export const ActiveUsersList = () => {
+const useUsers = () => {
   const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
@@ -10,15 +10,24 @@ export const ActiveUsersList = () => {
       const data = await response.json();
       setUsers(data);
     };
+
     loadUsers();
   }, []);
+
+  return { users };
+};
+
+export const ActiveUsersList = () => {
+  const { users } = useUsers();
 
   const weekAgo = new Date();
   weekAgo.setDate(weekAgo.getDate() - 7);
   return (
     <ul>
       {users
-        // .filter((user) => !user.isBanned && user.lastActivityAt >= weekAgo)
+        .filter(
+          (user) => !user.isBanned && new Date(user.lastActivityAt) >= weekAgo
+        )
         .map((user) => (
           <li key={user.id}>
             <img src={user.avatarUrl} />
