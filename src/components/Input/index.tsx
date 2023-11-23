8ac1c50a2eca206e5cdcd2fad973b7298e3daa10
Diego Oliveira de Araujo <diego.dearaujo@ambevtech.com.br>
2023-11-23 14:24:28 -0300

    LSP input example

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -16,4 +16,41 @@ const CharCountInput = (props: Props) => {
   );
 };
 
-export { Input, CharCountInput };
+const CustomInput = (props: Props) => {
+  // ...alguma lógica adicional
+
+  return <input {...props} />;
+};
+
+type CustomProps = InputHTMLAttributes<HTMLInputElement> & {
+  onUpdate: (value: string) => void;
+};
+
+const WrongCustomInput = ({ value, onChange }: CustomProps) => {
+  // este componente está errado, pois ao desestruturar perde todos os demais atributos
+  // ...alguma lógica adicional
+
+  return <input value={value} onChange={onChange} />;
+};
+
+const CorrectCustomInput = ({ onUpdate, ...props }: CustomProps) => {
+  // desestruturando o que precisa e mantem os demais, e repassa no componente
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    /// ... alguma lógica
+    onUpdate(event.target.value);
+  };
+
+  return <input {...props} onChange={onChange} />;
+};
+
+const BetterCustomInput = ({ onChange, ...props }: Props) => {
+  // é comum ter uma função local handleXXX correspondente para cada propriedade onXXX
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    /// ... alguma lógica
+    onChange?.(event);
+  };
+
+  return <input {...props} onChange={handleChange} />;
+};
+
+export { Input, CharCountInput, CustomInput };
