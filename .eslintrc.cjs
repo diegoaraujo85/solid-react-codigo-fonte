f51665ce83b6a762385cd0e38ca110aa7b4c3c09
Diego Oliveira de Araujo <diego.dearaujo@ambevtech.com.br>
2023-11-23 14:24:50 -0300

    lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,40 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "prettier"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "prettier/prettier": ["error"],
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+  "sort-keys": ["off", "asc"],
+  "import/no-cycle": "off",
+  "import/order": [
+    "warn",
+    {
+      groups: ["builtin", "external", "internal"],
+      pathGroups: [
+        {
+          pattern: "react",
+          group: "external",
+          position: "before",
+        },
+      ],
+      pathGroupsExcludedImportTypes: ["react"],
+      "newlines-between": "always",
+      alphabetize: {
+        order: "asc",
+        caseInsensitive: true,
+      },
+    },
+  ],
+};
